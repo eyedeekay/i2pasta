0f2e7d92be54732955d222ff0d710255092259e2
idk <problemsolver@openmailbox.org>
2018-04-16 03:21:46 -0400

    trying out standalone addresshelper client

--- addresshelper/addresshelper.go
+++ addresshelper/addresshelper.go
@@ -25,7 +25,7 @@ func (i *I2paddresshelper) fixUrl(addr string) string {
 	return addr
 }
 
-func (i *I2paddresshelper) getHostname(addr, jump string) string {
+func (i *I2paddresshelper) getHostinfo(addr, jump string) (string, string) {
 	resp, err := i.client.Get(i.fixUrl(addr))
 	if i.l.Error(err, "Sent request.") {
 		resp.Body.Close()
@@ -33,19 +33,20 @@ func (i *I2paddresshelper) getHostname(addr, jump string) string {
 			contents := strings.SplitN(location, "=", 2)
 			if len(contents) == 2 {
 				hostname := strings.Replace(strings.Replace(strings.Replace(contents[0], "http://", "", -1), "?i2paddresshelper", "", -1), "/", "", -1)
-				return hostname
+                b64 := contents[1]
+				return hostname, b64
 			}
 		}
 	}
 	return ""
 }
 
-func (i *I2paddresshelper) QueryHelper(addr string) string {
+func (i *I2paddresshelper) QueryHelper(addr string) (string, string) {
 	return i.getHostname(addr, i.jumpHost)
 
 }
 
-func (i *I2paddresshelper) QuerySHelper(addr, jump string) string {
+func (i *I2paddresshelper) QuerySHelper(addr, jump string) (string, string) {
 	return i.getHostname(addr, jump)
 }
 
