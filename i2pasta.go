7c03229703ee11b09a5094a0816e63eff1b3ca7b
idk <problemsolver@openmailbox.org>
2018-04-16 02:44:17 -0400

    starting standalone addresshelper client

--- i2pasta.go
+++ i2pasta.go
@@ -8,5 +8,5 @@ import (
 func test_import(){
     var i i2pconv.I2pconv
     i.I2p64to32("8ZAW~KzGFMUEj0pdchy6GQOOZbuzbqpWtiApEj8LHy2~O~58XKxRrA43cA23a9oDpNZDqWhRWEtehSnX5NoCwJcXWWdO1ksKEUim6cQLP-VpQyuZTIIqwSADwgoe6ikxZG0NGvy5FijgxF4EW9zg39nhUNKRejYNHhOBZKIX38qYyXoB8XCVJybKg89aMMPsCT884F0CLBKbHeYhpYGmhE4YW~aV21c5pebivvxeJPWuTBAOmYxAIgJE3fFU-fucQn9YyGUFa8F3t-0Vco-9qVNSEWfgrdXOdKT6orr3sfssiKo3ybRWdTpxycZ6wB4qHWgTSU5A-gOA3ACTCMZBsASN3W5cz6GRZCspQ0HNu~R~nJ8V06Mmw~iVYOu5lDvipmG6-dJky6XRxCedczxMM1GWFoieQ8Ysfuxq-j8keEtaYmyUQme6TcviCEvQsxyVirr~dTC-F8aZ~y2AlG5IJz5KD02nO6TRkI2fgjHhv9OZ9nskh-I2jxAzFP6Is1kyAAAA")
-    ah := NewI2pAddressHelper("http://inr.i2p", "127.0.0.1", 7656 )
+    ah := i2paddresshelper.NewI2pAddressHelper("http://inr.i2p", "127.0.0.1", 7656 )
 }
