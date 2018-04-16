1651a17dbdf7a0aa354507ab8d9ee9037029046a
idk <problemsolver@openmailbox.org>
2018-04-16 02:56:12 -0400

    setup sam bridge in standalone addresshelper client

--- addresshelper/addresshelper.go
+++ addresshelper/addresshelper.go
@@ -30,5 +30,21 @@ func (i *I2paddresshelper) QuerySHelper(addr, jump string){
 
 func NewI2pAddressHelper(jump string, host... string) *I2paddresshelper{
     var i I2paddresshelper
+    if len(host) == 1 {
+        i.samclient = goSam.NewClient(host[0] + ":7656")
+    }else if len(host) == 2 {
+        i.samclient = goSam.NewClient(host[0] + ":" + host[1])
+    }
+    i.jumpHost = jump
+    i.transport = &http.Transport{
+		Dial: i.samclient.Dial,
+	}
+	Log("i2pdig.go Setting up Client")
+	i.client = &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+		Transport: i.transport,
+	}
     return &i
 }
