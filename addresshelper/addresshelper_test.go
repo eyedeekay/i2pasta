a548860b5af6c9d1f127f5cdec42af546369b9bd
idk <problemsolver@openmailbox.org>
2018-06-06 20:59:17 -0400

    that'll break something.

--- addresshelper/addresshelper_test.go
+++ addresshelper/addresshelper_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestAddressHelper(t *testing.T) {
-	addresshelper, err := NewI2pAddressHelper("http://inr.i2p", "127.0.0.1", "7656")
+	addresshelper, err := NewI2pAddressHelper("http://inr.i2p", "127.0.0.1", "7656", true)
 	if err != nil {
 		log.Fatal(err)
 	}
