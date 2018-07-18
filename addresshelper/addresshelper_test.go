61a01d16ded07bfe8b5dae33cc57922c5ea41830
idk <problemsolver@openmailbox.org>
2018-07-18 17:34:41 -0400

    use b32 url in tests

--- addresshelper/addresshelper_test.go
+++ addresshelper/addresshelper_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestAddressHelper(t *testing.T) {
-	addresshelper, err := NewI2pAddressHelper("http://inr.i2p", "127.0.0.1", "7656", true)
+	addresshelper, err := NewI2pAddressHelper("http://joajgazyztfssty4w2on5oaqksz6tqoxbduy553y34mf4byv6gpq.b32.i2p", "127.0.0.1", "7656", true)
 	if err != nil {
 		log.Fatal(err)
 	}
